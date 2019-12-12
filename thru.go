62e9d254021c8f421b83beb4cce6fa140bbcd870
Keith <keith@keithmackay.com>
2019-12-11 22:18:57 -0500

    Exit immediately if not through pipe

--- thru.go
+++ thru.go
@@ -7,23 +7,30 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
-	var file *bufio.Writer
-	if len(args) > 0 {
-		fileName := args[0]
-		f, _ := os.Create(fileName)
-		file = bufio.NewWriter(f)
-		if file != nil {
-			defer file.Flush()
+	fi, _ := os.Stdin.Stat()
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		// Input is being piped in
+		args := os.Args[1:]
+		var file *bufio.Writer
+		if len(args) > 0 {
+			fileName := args[0]
+			f, _ := os.Create(fileName)
+			file = bufio.NewWriter(f)
+			if file != nil {
+				defer file.Flush()
+			}
 		}
-	}
-	reader := bufio.NewScanner(os.Stdin)
-	for reader.Scan() {
-		text := reader.Text()
-		fmt.Println(text)
-		if file != nil {
-			_, _ = file.WriteString(text + "\n")
-			go file.Flush()
+		reader := bufio.NewScanner(os.Stdin)
+		for reader.Scan() {
+			text := reader.Text()
+			fmt.Println(text)
+			if file != nil {
+				_, _ = file.WriteString(text + "\n")
+				go file.Flush()
+			}
 		}
+	} else {
+		// Input is not coming in through Pipe
+		return
 	}
 }
